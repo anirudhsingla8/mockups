d097d2f93d676b94eed4cd63034ad469297dcea2
anirudhsingla8 <anirudhsingla8@gmail.com>
2017-02-18 12:22:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles={ 
-articleone:{
+
+var articleone={
   title:'article-one | Anirudh Singla',
   heading: 'Article one',
   date:'feb 18,2016',
@@ -20,46 +20,20 @@ articleone:{
              <p>
              this is the content for my first article.
              </p>`
-},
-articletwo:{ title:'article-two | Anirudh Singla',
-  heading: 'Article two',
-  date:'feb 18,2016',
-  content:  `
-             <p>
-             this is the content for my second article.
-             </p>
-             <p>
-             this is the content for my second article.
-             </p>
-             <p>
-             this is the content for my second article.
-             </p>`},
-articlethree:{ title:'article-three | Anirudh Singla',
-  heading: 'Article three',
-  date:'feb 18,2016',
-  content:  `
-             <p>
-             this is the content for my third article.
-             </p>
-             <p>
-             this is the content for my third article.
-             </p>
-             <p>
-             this is the content for my third article.
-             </p>`},
 };
-function createTemplate(data)
-{
- var title=data.title;
- var date=data.date;
- var heading=data.heading;
- var content=data.content;
-var htmlTemplate=
-`
+
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+}
+var htmlTemplate=`
 <html>
     <head>
       <title>
-        ${title}
+         ${title}
       </title>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link href="/ui/style.css" rel="stylesheet" />
@@ -86,6 +60,13 @@ var htmlTemplate=
 `;
 return htmlTemplate;
 }
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
