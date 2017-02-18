948cce754067be53c0c98e59c0de7bdfb9789503
anirudhsingla8 <anirudhsingla8@gmail.com>
2017-02-18 11:09:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content=
+{
+  title:'article-one | Anirudh Singla',
+  heading: 'Article one',
+  date:'feb 18,2016',
+  content:  '
+             <p>
+             this is the content for my first article.
+             </p>
+             <p>
+             this is the content for my first article.
+             </p>
+             <p>
+             this is the content for my first article.
+             </p>'
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
