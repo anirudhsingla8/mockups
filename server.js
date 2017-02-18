d3ec9178d127b1885f29e131d8d4f62a690b372d
anirudhsingla8 <anirudhsingla8@gmail.com>
2017-02-18 11:54:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,12 +95,12 @@ app.get('/article-one', function(req, res) {
 });
 
    app.get('/article-two', function(req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+   res.send(createTemplate(articletwo));
 });
 
 
 app.get('/article-three', function(req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+   res.send(createTemplate(articlethree));
 });
 
 app.get('/ui/style.css', function (req, res) {
