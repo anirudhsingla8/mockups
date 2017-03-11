3295033fb8d96831411b6ffe751305018821d234
anirudhsingla8 <anirudhsingla8@gmail.com>
2017-03-12 03:14:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    user:'anirudhsingla8',
+    database:'anirudhsingla8',
+    host:'db.imad.hasura-ap.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+    };
 var app = express();
 app.use(morgan('combined'));
 
@@ -92,6 +99,19 @@ return htmlTemplate;
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('test-db'), function(req,res){
+    //take a result request
+    //return a response with result
+    pool.query('SELECT * FROM  test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringify(result));
+       }
+    });
+};
 
 var counter = 0;
 app.get('/counter', function(req, res){
