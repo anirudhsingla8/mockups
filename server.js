4876ae9bc6abfa6acd4b05c3b2f91e9c58e80ae2
anirudhsingla8 <anirudhsingla8@gmail.com>
2017-03-12 14:31:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 var config = {
     user:'anirudhsingla8',
     database:'anirudhsingla8',
-    host:'db.imad.hasura-ap.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
     };
